500941e678ae1faa2ef78bfb92e538bd9b9dae44
FORSWAROOPRAJ <forswaroopraj@gmail.com>
2022-04-22 18:32:54 +0530

    make it better

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
-    'http://localhost:3000',
+    'https://git.heroku.com/jwtpraticeguvi.git',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'https://6262a234dafa2c1a8ce0361c--genuine-sunburst-75ac4f.netlify.app/login'
+    'https://genuine-sunburst-75ac4f.netlify.app'
     
 ];
 

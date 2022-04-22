5bc81f69839935fe4d8c622b0de7550f2b2d42e7
FORSWAROOPRAJ <forswaroopraj@gmail.com>
2022-04-22 18:12:46 +0530

    make it better

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
+    'https://6262a234dafa2c1a8ce0361c--genuine-sunburst-75ac4f.netlify.app/login'
     
 ];
 
